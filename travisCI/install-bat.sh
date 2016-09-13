755d1823ae93522315da4696d84a5559fee919b9
pdigiglio <p.digiglio91@gmail.com>
2016-09-13 15:12:34 +0200

    [travis-conf] set BAT environment

--- travisCI/install-bat.sh
+++ travisCI/install-bat.sh
@@ -14,5 +14,11 @@ git checkout bede80ec026b381e12b5436f7cc05d12c1ec07c7
 ./configure --prefix=$PWD --enable-parallel
 
 # compile and install
-make
+make -j
 make install
+
+# Set the environment
+export PATH="/home/travis/build/pdigiglio/YAP/bat/bin:$PATH"
+export LD_LIBRARY_PATH="/home/travis/build/pdigiglio/YAP/bat/lib:$LD_LIBRARY_PATH"
+export CPATH="/home/travis/build/pdigiglio/YAP/bat/include:$CPATH"
+export PKG_CONFIG_PATH="/home/travis/build/pdigiglio/YAP/bat/lib/pkgconfig:$PKG_CONFIG_PATH"
